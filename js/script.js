5746720f62ed8ee3e7e972dddba0f0f04320d2ca
Shahinur Rahman Shahin <srshahin111@gmail.com>
2025-05-05 00:45:34 +0600

    make sticky navigation

--- js/script.js
+++ js/script.js
@@ -26,3 +26,24 @@ allLinks.forEach(function (link) {
     }
   });
 });
+
+// sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) document.body.classList.add("sticky");
+
+    if (ent.isIntersecting) document.body.classList.remove("sticky");
+  },
+
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
