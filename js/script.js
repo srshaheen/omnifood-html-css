976749d29b93ebf0b04a5bb106991e73907c779d
Shahinur Rahman Shahin <srshahin111@gmail.com>
2025-05-05 00:24:23 +0600

    refactoring some code and make smooth

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const btnEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnEl.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
+});
+
+// scroll behavior
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
